eed55980606a6d46bfeffd5c8969a56d3243656e
NiklasHammerstone <niklas.hammerschmidt@gmx.de>
2021-03-20 14:18:24 +0100

    Speeds and feeds now work correctly

--- Codes/GalvoStepV1_Firmware/GalvoStepV1_Firmware.ino
+++ Codes/GalvoStepV1_Firmware/GalvoStepV1_Firmware.ino
@@ -16,17 +16,18 @@
 #define endswitchY 20
 #define homePosX 1470  //Tweak this to get a perfect 45 deg angle as 0 position
 #define homePosY 1350  //Tweak this to get a perfect 45 deg angle as 0 position
-#define homeSpeed 500
+#define homeSpeed 1500
 #define INTERPOLATION 20    //Radius moves will be approximated by n=INTERPOLATION linear submoves
   
-int jogSpeed = 13000;   //Default value for G0 jog Speed (step per second)
-int workSpeed = 2500;  //Default value for G1 work Speed (step per second)
+int jogSpeed = 500;   //Default value for G0 jog Speed (step per second)
+int workSpeed = 100;  //Default value for G1 work Speed (step per second)
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+#define SCB_AIRCR (*(volatile uint32_t *)0xE000ED0C) // Application Interrupt and Reset Control location
+
   int steps_per_rot = 200*micro_step;
   double degrees_per_step = 360.00 / steps_per_rot;
-  
   struct XY{
     double x;
     double y;
@@ -34,24 +35,17 @@ int workSpeed = 2500;  //Default value for G1 work Speed (step per second)
 
   AccelStepper Xaxis(1, step_x, dir_x); 
   AccelStepper Yaxis(1, step_y, dir_y);
-  MultiStepper steppers;
 
   const byte numChars = INPUT_SIZE;
   char receivedChars[numChars]; // an array to store the received data, max length 20
   boolean newData = false;
+  byte workMode = 0;
 
   struct XY currPos;
   
   struct XY saved;
   
 void setup() {
-
-  Xaxis.setAcceleration(1000000);   //Acceleration must be very high
-  Xaxis.setMaxSpeed(jogSpeed);  
-  Yaxis.setAcceleration(1000000);   //Acceleration must be very high
-  Yaxis.setMaxSpeed(jogSpeed);  
-  steppers.addStepper(Xaxis);
-  steppers.addStepper(Yaxis);
 
   pinMode(endswitchX, INPUT);
   pinMode(endswitchY, INPUT);
@@ -73,11 +67,11 @@ void loop() {
     newData = false;}
   if (digitalRead(endswitchY)==HIGH){
     Serial.println("Y has been triggered");
-    delay(500);
+    delay(200);
   }
   if (digitalRead(endswitchX)==HIGH){
     Serial.println("X has been triggered");
-    delay(500);
+    delay(200);
   }
 
 }
@@ -88,11 +82,20 @@ void move_To(double x, double y){
   angle.y = (atan(y/D) / 2 ) * 57.2957795131 ;
   angle.x = (atan( x / (E + sqrt(pow(D, 2) + pow(y, 2)) ) ) / 2 ) * 57.2957795131 ;
   long step_pos[2];
-  step_pos[0] = (int)(angle.x / degrees_per_step);
-  step_pos[1] = (int)(angle.y / degrees_per_step);
+  step_pos[0] = (long)(angle.x / degrees_per_step);
+  step_pos[1] = (long)(angle.y / degrees_per_step);
+  MultiStepper steppers;  
+  if (workMode == 0){
+    Xaxis.setMaxSpeed(jogSpeed);
+    Yaxis.setMaxSpeed(jogSpeed);
+  }else{
+    Xaxis.setMaxSpeed(workSpeed);
+    Yaxis.setMaxSpeed(workSpeed);
+  }
+  steppers.addStepper(Xaxis);
+  steppers.addStepper(Yaxis);
   steppers.moveTo(step_pos);
   steppers.runSpeedToPosition();
-
 }
 
 void readGCode(){
@@ -103,7 +106,7 @@ void readGCode(){
   double Commands[sizeof(CommandTypes)]={0};
   int CommandIndeces[sizeof(CommandTypes)]={0};
 
-  for (int j=0;j<sizeof(CommandTypes);j++){
+  for (unsigned int j=0;j<sizeof(CommandTypes);j++){
     
     CommandIndeces[j] = indexof(receivedChars, CommandTypes[j]);
     if (CommandIndeces[j] != -1){
@@ -136,27 +139,22 @@ void readGCode(){
     switch((int) Commands[0]){
     case 0:
       if (CommandIndeces[6]!=-1){
-         int linSpeed = (int) Commands[6];
-         jogSpeed = linSpeed;
-         Xaxis.setMaxSpeed(linSpeed);
-         Yaxis.setMaxSpeed(linSpeed);
-      }else{
-         Xaxis.setMaxSpeed(jogSpeed);
-         Yaxis.setMaxSpeed(jogSpeed);
-        }
+         jogSpeed = (int) Commands[6];
+      } 
+      workMode = 0;
       move_To(pos.x, pos.y);
+      Serial.println("Moved laser");
       Serial.println("OK");
       break;
     case 1:
       if (CommandIndeces[6]!=-1){
-         int linSpeed = (int) Commands[6];
-         workSpeed = linSpeed;
-         Xaxis.setMaxSpeed(linSpeed);
-         Yaxis.setMaxSpeed(linSpeed);
-      }else{
-         Xaxis.setMaxSpeed(jogSpeed);
-         Yaxis.setMaxSpeed(jogSpeed);}
+         workSpeed = (int) Commands[6];
+      }
+      workMode = 1;
+      delay(50);
       move_To(pos.x, pos.y);
+      delay(10);
+      Serial.println("Moved laser");
       Serial.println("OK");
       break;
     case 2:
@@ -165,6 +163,7 @@ void readGCode(){
       if (CommandIndeces[5]==-1){
         Commands[5]=0;}
       moveArc(true, Commands[1], Commands[2], Commands[4], Commands[5]);
+      Serial.println("Moved laser");
       Serial.println("OK");
       break;
     case 3:
@@ -173,25 +172,35 @@ void readGCode(){
       if (CommandIndeces[5]==-1){
         Commands[5]=0;}
       moveArc(false, Commands[1], Commands[2], Commands[4], Commands[5]);
+      Serial.println("Moved laser");
       Serial.println("OK");
       break;
     case 28:
       homing();
       Serial.println("Mirrors homed successfully.");
+      Serial.println("OK");
       break;
     case 60:
       saved.x = currPos.x;
       saved.y = currPos.y;
       Serial.println("Saved the current position");
+      Serial.println("OK");
       break;
     case 61:
       pos.x = saved.x;
       pos.y = saved.y;
       move_To(pos.x, pos.y);
       Serial.print("Remembered X:"); Serial.print(pos.x); Serial.print(", Y: "); Serial.println(pos.y);
+      Serial.println("OK");
+      break;
+    case 99:
+      Serial.println("RESETTING");
+      Serial.end();  //clears the serial monitor  if used
+      SCB_AIRCR = 0x05FA0004;  //write value for restart
       break;
     default:
       Serial.println("Command unknown. I'll skip this one.");
+      Serial.println("OK");
       break;
   }
   }else{
@@ -199,36 +208,35 @@ void readGCode(){
       case 3:
         digitalWrite(LASER, HIGH);
         Serial.println("LASER ON");
+        Serial.println("OK");
         break;
       case 4:
         digitalWrite(LASER, HIGH);
         Serial.println("LASER ON");
+        Serial.println("OK");
         break;
       case 5:
         digitalWrite(LASER, LOW);
         Serial.println("LASER OFF");
+        Serial.println("OK");
         break;
       case 6:
         Serial.println("Firing the Laser for 100ms");
         digitalWrite(LASER, HIGH);
         delay(100);
         digitalWrite(LASER, LOW);
+        Serial.println("OK");
         break;
-      case 201: //Usage: M201 X[Accel] Y[Accel]
-        int maxAccelX = (int) Commands[1];
-        int maxAccelY = (int) Commands[2];
-        Xaxis.setAcceleration(maxAccelX);
-        Yaxis.setAcceleration(maxAccelY);           
-        Serial.println("Max Acceleration has been set.");
-      case 203: //Usage: M203 X[maxSpeed] Y[maxSpeed]
-        int maxSpeedX = (int) Commands[1];
-        int maxSpeedY = (int) Commands[2];
-        Xaxis.setMaxSpeed(maxSpeedX);
-        Yaxis.setMaxSpeed(maxSpeedY);           
-        Serial.println("Max Speed has been set.");
+      case 201: //Usage: M201 X[Accel] Y[Accel]      
+        Serial.println("Acceleration is deprecated.");
+        Serial.println("OK");
+      case 203: //Usage: M203 X[maxSpeed] Y[maxSpeed]        
+        Serial.println("Max Speed is deprecated.");
+        Serial.println("OK");
       break;
     default:
       Serial.println("Command unknown. I'll skip this one.");
+      Serial.println("OK");
       break;        
     }
   }
@@ -245,7 +253,6 @@ void recvWithEndMarker() {
 
  while (Serial.available() > 0 && newData == false) {
    rc = Serial.read();
-  
    if (rc != endMarker) {
       receivedChars[ndx] = rc;
       ndx++;
@@ -257,11 +264,12 @@ void recvWithEndMarker() {
      ndx = 0;
      newData = true;}
    }
+  
 }
 
 int indexof(char str[], char c){
   //Returns the index of a char in a chararray. 
-  int j = 0; 
+  byte j = 0; 
   boolean found = false;
   while(j<numChars){
         if (str[j]!=c){j++; }
@@ -273,10 +281,15 @@ int indexof(char str[], char c){
 }
 
 void homing(){
-  
-  Xaxis.moveTo(-steps_per_rot);
-  Xaxis.setSpeed(-homeSpeed);
 
+  int prevXmaxSpeed = Xaxis.maxSpeed();
+  int prevYmaxSpeed = Yaxis.maxSpeed();
+  int prevXspeed = Xaxis.speed();
+  int prevYspeed = Yaxis.speed();
+  Xaxis.setMaxSpeed(homeSpeed);
+  Yaxis.setMaxSpeed(homeSpeed);
+  
+  Xaxis.setSpeed(-homeSpeed);
 
   while(digitalRead(endswitchX)==LOW){
     Xaxis.runSpeed();
@@ -286,13 +299,10 @@ void homing(){
   Xaxis.moveTo(homePosX);
   Xaxis.setSpeed(homeSpeed);
 
-
   while(Xaxis.distanceToGo() != 0){
-    Xaxis.runSpeed();
-  }
+    Xaxis.runSpeed();}
   Xaxis.setCurrentPosition(0);
 
- Yaxis.moveTo(-steps_per_rot);
  Yaxis.setSpeed(-homeSpeed);
 
   while(digitalRead(endswitchY)==LOW){
@@ -302,12 +312,14 @@ void homing(){
   Yaxis.setCurrentPosition(0);
   Yaxis.moveTo(homePosY);
   Yaxis.setSpeed(homeSpeed);
-
-
   while(Yaxis.distanceToGo() != 0){
-    Yaxis.runSpeed();
-  }
+    Yaxis.runSpeed();}
   Yaxis.setCurrentPosition(0);
+  
+  Xaxis.setMaxSpeed(prevXmaxSpeed);
+  Yaxis.setMaxSpeed(prevYmaxSpeed);
+  Xaxis.setSpeed(prevXspeed);
+  Yaxis.setSpeed(prevYspeed);
   currPos.x = 0.0;
   currPos.y = 0.0;
 }
